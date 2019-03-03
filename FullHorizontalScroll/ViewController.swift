884fe09fe6ccfbd0450322b2ac5d1e6d8894ef11
wirotechnologies <wrojas@wirotechnologies.com>
2019-03-02 20:46:47 -0500

    Initial Commit

---
+++ FullHorizontalScroll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FullHorizontalScroll
+//
+//  Created by Wilman Rojas on 3/2/19.
+//  Copyright © 2019 Wiro Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
